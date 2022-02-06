33a555043457095736575015850373933fa52f59
Crowdbotics <team@crowdbotics.com>
2022-02-06 16:39:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "kindlab_33401",
+  url: "https://kindlab_33401.botics.co",
+  api: "https://kindlab_33401.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
